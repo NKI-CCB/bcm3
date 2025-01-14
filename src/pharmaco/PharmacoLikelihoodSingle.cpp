179b78265f4f8fb8d2d065c1945ff5af0f337e34
Bram Thijssen <b.thijssen@nki.nl>
2025-01-14 20:11:53 +0100

    Add intermittent treatments and dose changes

--- src/pharmaco/PharmacoLikelihoodSingle.cpp
+++ src/pharmaco/PharmacoLikelihoodSingle.cpp
@@ -74,25 +74,73 @@ bool PharmacoLikelihoodSingle::Initialize(std::shared_ptr<const bcm3::VariableSe
 		return false;
 	}
 
+	Real dose_after_dose_change;
+	Real dose_change_time;
+	unsigned int intermittent;
+	std::set<int> skipped_days;
+
 	observation_timepoints.resize(num_timepoints);
 	observed_concentrations.resize(num_timepoints);
 	result &= data.GetValues(trial, "time", 0, num_timepoints, observation_timepoints);
 	result &= data.GetValuesDim2(trial, drug + "_plasma_concentration", patient_ix, 0, num_timepoints, observed_concentrations);
 	result &= data.GetValue(trial, drug + "_dose", patient_ix, &dose);
 	result &= data.GetValue(trial, drug + "_dosing_interval", patient_ix, &dosing_interval);
+	result &= data.GetValue(trial, drug + "_dose_after_dose_change", patient_ix, &dose_after_dose_change);
+	result &= data.GetValue(trial, drug + "_dose_change_time", patient_ix, &dose_change_time);
+	result &= data.GetValue(trial, drug + "_intermittent", patient_ix, &intermittent);
+
+	std::vector<unsigned int> interruptions;
+	for (int i = 0; i < 29; i++) {
+		unsigned int tmp;
+		result &= data.GetValue(trial, "treatment_interruptions", patient_ix, (size_t)i, &tmp);
+		if (tmp) {
+			skipped_days.insert(i);
+		}
+	}
 
 	std::vector<Real> times;
 	Real last_time = 696;
 	Real t = 0;
 	while (t < last_time) {
-		times.push_back(t);
+		bool give_treatment = true;
+		int day = static_cast<int>(floor(t / 24.0));
+		if (skipped_days.find(day) != skipped_days.end()) {
+			give_treatment = false;
+		}
+		if (intermittent == 1) {
+			Real time_in_week = t - 7.0 * 24.0 * floor(t / (7.0 * 24.0));
+			if (time_in_week >= 5.0 * 24.0) {
+				// No dose in day 6 and 7
+				give_treatment = false;
+			}
+		} else if (intermittent == 2) {
+			Real time_in_treatment_course = t - 28.0 * 24.0 * floor(t / (28.0 * 24.0));
+			if (time_in_treatment_course >= 21.0 * 24.0) {
+				// No dose in day 22 through 28
+				give_treatment = false;
+			}
+		} else if (intermittent == 3) {
+			Real time_in_week = t - 7.0 * 24.0 * floor(t / (7.0 * 24.0));
+			if (time_in_week >= 4.0 * 24.0) {
+				// No dose in day 5, 6 and 7
+				give_treatment = false;
+			}
+		}
+
+		if (give_treatment) {
+			times.push_back(t);
+		}
 		t += dosing_interval;
 	}
 	treatment_timepoints.resize(times.size());
 	treatment_doses.resize(times.size());
 	for (ptrdiff_t i = 0; i < times.size(); i++) {
 		treatment_timepoints(i) = times[i];
-		treatment_doses(i) = dose;
+		if (!std::isnan(dose_change_time) && times[i] >= dose_change_time) {
+			treatment_doses(i) = dose_after_dose_change;
+		} else {
+			treatment_doses(i) = dose;
+		}
 	}
 
 	VectorReal new_observed_timepoints = observation_timepoints;
